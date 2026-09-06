3c347867691a86ae6739faaadad9bb4707288a27
JivniCare <jivnicare26@gmail.com>
2026-09-06 22:38:53 +0530

    chore(patient-mobile): configure package name com.jivnicare.app, display name JivniCare, and jivnicare scheme

--- android/app/src/main/java/com/jivnicare/app/MainActivity.kt
+++ android/app/src/main/java/com/jivnicare/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.jivnicare.partner
+package com.jivnicare.app
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/jivnicare/app/MainApplication.kt
+++ android/app/src/main/java/com/jivnicare/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.jivnicare.partner
+package com.jivnicare.app
 
 import android.app.Application
 import android.content.res.Configuration
